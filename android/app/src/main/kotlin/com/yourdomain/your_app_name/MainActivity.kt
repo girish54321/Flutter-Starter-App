4e7747b1e92901a3eee48378cd16b1710ff14ddb
Girish Parate <girishparate@Girishs-MacBook-Pro.local>
2026-01-03 21:06:26 +0530

    Native code added for Build Type

--- android/app/src/main/kotlin/com/yourdomain/your_app_name/MainActivity.kt
+++ android/app/src/main/kotlin/com/yourdomain/your_app_name/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.yourdomain.your_app_name
 
+import android.content.Context
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.nativebridge.*
+import io.nativebridge.FlutterNativeBridge
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        FlutterNativeBridge.register(this)
+        val buildService = BuildService(applicationContext)
+    }
+}
+
+@NativeBridge
+class BuildService (private val context: Context){
+    fun getBuildType(): String = context.getString(R.string.BUILD_ENV)
+}
